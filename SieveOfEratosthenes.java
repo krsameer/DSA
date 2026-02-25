87c22e19c8783f754c186cbb2dcc27bdf4834af2
krsameer <skateboard.maniac18@gmail.com>
2026-02-25 19:12:06 +0530

    Modified

--- SieveOfEratosthenes.java
+++ SieveOfEratosthenes.java
@@ -1,3 +1,12 @@
+// Approach
+// Create a boolean array of size n+1 and initialize all entries as true. A value in the boolean array will finally be false if it is Not a prime, else true.
+// Start with the first prime number, which is 2. Mark all of its multiples as false
+// Repeat the process for the next number in the boolean array. If it is true, then it is a prime number. Mark all of its multiples as false.
+// Continue the process until you have processed numbers up to √n. The remaining true values in the
+// boolean array will be prime numbers.
+// Time Complexity: O(n log log n) for generating all prime numbers up to n.
+// Space Complexity: O(n) for the boolean array to keep track of prime numbers.
+
 import java.util.*;
 
 class SieveOfEratosthenes {
