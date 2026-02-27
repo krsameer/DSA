6315d1630ece5907244101dc21fc529b5db054e3
krsameer <skateboard.maniac18@gmail.com>
2026-02-27 15:49:26 +0530

    Added Linear Search program

---
+++ LinearSearch.java
@@ -0,0 +1,46 @@
+//Time Complexity: O(n) for searching an element in the array.
+//Space Complexity: O(1) for using constant extra space.
+
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Array size
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input elements
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Element to search
+        System.out.print("Enter element to search: ");
+        int key = sc.nextInt();
+
+        boolean found = false;
+
+        // Linear Search Algorithm
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == key) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
