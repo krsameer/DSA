578069a4441849e22d777829511648dc08179e66
krsameer <skateboard.maniac18@gmail.com>
2026-04-18 18:12:35 +0530

    Added preorder traversal in binary tree

---
+++ PreorderTraversal.java
@@ -0,0 +1,2 @@
+// Given the root of a binary tree, your task is to return its Preorder traversal.
+// Note: A preorder traversal first visits the node, then visits the left child (including its entire subtree), and finally visits the right child (including its entire subtree).
