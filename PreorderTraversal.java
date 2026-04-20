ad26e6b1a72bd12b29f48c389480f6b53c2786dc
krsameer <skateboard.maniac18@gmail.com>
2026-04-20 19:31:38 +0530

    Modified Preorder Traversal

--- PreorderTraversal.java
+++ PreorderTraversal.java
@@ -1,2 +1,29 @@
 // Given the root of a binary tree, your task is to return its Preorder traversal.
 // Note: A preorder traversal first visits the node, then visits the left child (including its entire subtree), and finally visits the right child (including its entire subtree).
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+    Node(int data) {
+        this.data=data;
+        this.right=null;
+        this.left=null;
+    }
+}
+
+class PreorderTraversal {
+    public ArrayList<Integer> preOrder(Node root) {
+        ArrayList<Integer> result=new ArrayList<>();
+        if(root==null)
+        return result;
+        ans(root,result);
+        return result;
+    }
+    void ans(Node node, ArrayList<Integer> result) {
+        if (node == null) return;
+        result.add(node.data);
+        ans(node.left, result);
+        ans(node.right, result);
+    }
+}
