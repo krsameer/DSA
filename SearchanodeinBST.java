7945c888194cd5192b08dab63f14f408e7aaf19a
krsameer <skateboard.maniac18@gmail.com>
2026-04-21 23:59:15 +0530

    Search a node in bst

---
+++ SearchanodeinBST.java
@@ -0,0 +1,15 @@
+// 
+
+class SearchanodeinBST {
+    public boolean search(Node root, int key) {
+        if(root==null)
+        return false;
+        if(root.data==key)
+        return true;
+        if(key<root.data) {
+        return search(root.left,key);
+    } else {
+        return search(root.right,key);
+    }
+    }
+}
