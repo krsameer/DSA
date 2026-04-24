7f06b81c8140d6cc7d30b681b2b8108256abc38f
krsameer <skateboard.maniac18@gmail.com>
2026-04-24 22:18:15 +0530

    left view

--- LeftViewOfBinaryTree.java
+++ LeftViewOfBinaryTree.java
@@ -1,6 +1,8 @@
+// Left View of Binary Tree
+// Given a binary tree, return an array containing the left view of the binary tree. The left view of a binary tree is the set of nodes visible when the tree is viewed from the left side. 
+
+
 import java.util.ArrayList;
-
-
 class Node {
     int data;
     Node left, right;
